40d8f0d8214d9df4df2b7d2ef7d795854f19e1eb
Wade <huzzabari@gmail.com>
2024-03-27 12:53:56 -0400

    I will handle each view post request seperately.

--- auctions/views.py
+++ auctions/views.py
@@ -80,35 +80,48 @@ def create(request):            # post request takes the form data and makes a f
     form=AuctionForm()              # otherwise it just goes to the create page and creates a form for the auction
     return render(request,"auctions/create.html", {"form":form})
 
-def listing(request, pk):                        # If the listing has a post request it creates a bid form with a csrf token and checks if the form data is valid
-    if 'form1' in request.POST:
-        form1=BidForm(request.POST)               # request bid form object.
-        auction=Auction.objects.get(pk=pk)         # get auction object class for the primary key
-        if form1.is_valid():
-            new_bid=form1.cleaned_data["new_bid"]                # if the form is valid the new bid field gets the bidform clean data of new bid
-            if new_bid >= auction.starting_bid and new_bid > auction.highest_bid:       # if new bid is higher then save it to the auction form and go back to index.
-                auction.highest_bid=new_bid
-                auction.save()
-                return HttpResponseRedirect(reverse("index"))
+
+def listing(request, pk):
+    print("Request method:", request.method)  # Check the request method
+    if request.method == "POST":
+        if 'form1' in request.POST:
+            print("Form1 submitted")  # Check if this block is being entered
+            form1 = BidForm(request.POST)
+            auction = Auction.objects.get(pk=pk)
+            if form1.is_valid():
+                new_bid = form1.cleaned_data["new_bid"]
+                print("New bid:", new_bid)
+                print("Starting bid:", auction.starting_bid)
+                print("Current highest bid:", auction.highest_bid)
+                if new_bid >= auction.starting_bid and new_bid > auction.highest_bid:
+                    auction.highest_bid = new_bid
+                    auction.save()
+                    print("Highest bid updated successfully")
+                    return HttpResponseRedirect(reverse("index"))
+                else:
+                    print("Bid is not higher than starting bid or current highest bid")
+                    print(form1.errors)
+            else:
+                print("Form is not valid")
+                
+        elif 'form2'in request.POST:                                 # made a change to have 2 forms.  form two post does something different.
+            form2=CommentForm(request.POST)
+            if form2.is_valid():                  
+                new_comment=form2.cleaned_data["comments"]
+                user_id = UserProfile.objects.get(user=request.user)
+                comments=Comments.objects.create(auction_id=pk, user_id=user_id, comments=new_comment)
+                print(comments)
+                return HttpResponseRedirect(reverse("listing", args=[pk]))
             
-    elif 'form2'in request.POST:                                 # made a change to have 2 forms.  form two post does something different.
-        form2=CommentForm(request.POST)
-        if form2.is_valid():                  
-            new_comment=form2.cleaned_data["comments"]
-            user_id = UserProfile.objects.get(user=request.user)
-            comments=Comments.objects.create(auction_id=pk, user_id=user_id, comments=new_comment)
-            print(comments)
-            return HttpResponseRedirect(reverse("listing", args=[pk]))
-        
-        user_profile= UserProfile.objects.get(user=request.user)  #get user profile instance
-        watchlist=user_profile.watchlist.all()                # get everything from the users watchlist
-        auction=Auction.objects.get(pk=pk)                        # get the instance of the auction the user is looking at
-        if auction in watchlist:                              # if the auction is in the watchlist during a post request then remove it, otherwise add it.
-            user_profile.watchlist.remove(auction)
+            user_profile= UserProfile.objects.get(user=request.user)  #get user profile instance
+            watchlist=user_profile.watchlist.all()                # get everything from the users watchlist
+            auction=Auction.objects.get(pk=pk)                        # get the instance of the auction the user is looking at
+            if auction in watchlist:                              # if the auction is in the watchlist during a post request then remove it, otherwise add it.
+                user_profile.watchlist.remove(auction)
+                return HttpResponseRedirect(reverse("index"))
+            user_profile.watchlist.add(auction)
             return HttpResponseRedirect(reverse("index"))
-        user_profile.watchlist.add(auction)
-        return HttpResponseRedirect(reverse("index"))
-        
+            
     #GET REQUEST    
     user=request.user                                                   # Otherwise it creates a bid form that has the auction id and user info hidden
     try:
@@ -121,6 +134,17 @@ def listing(request, pk):                        # If the listing has a post req
     form1 = BidForm(initial={'auction_id': auction.id,"user":user.id})
     form2=CommentForm(initial={'auction_id': auction.id,"user":user.id})               
     return render(request, "auctions/listing.html", {'auction':auction,"form1":form1, "form2":form2, 'watchlist': watchlist, "comments":comments})
+
+
+def handle_form1(request, pk):
+    pass
+
+def handle_form2(request, pk):
+    pass
+
+def handle_watchlist(request,pk):
+    pass
+
 
 def watchlist(request):    #watchlist should be a post request to save the info, and a get request to view a users watchlist
     user_profile= UserProfile.objects.get(user=request.user)
